74a3f41f949a5daf97a5f8b58dab009379459682
junhoheo <junhoheo@192.168.219.108>
2023-05-18 14:43:27 +0900

    05/18 14:42 11번 해석,필기연습.

--- 04_java_string/src/step4_01/string/StringEx11_필기.java
+++ 04_java_string/src/step4_01/string/StringEx11_필기.java
@@ -16,27 +16,29 @@ import java.util.Scanner;
  * 		{"음료", "콜라/"},
  * 		{"육류", "소고기/"}
  * 		...
+ * 
+ * 
  * } 
  */
 
 
-public class StringEx11_정답예시 {
+
+public class StringEx11_필기 {
 
 	public static void main(String[] args) {
-
+		
 		Scanner scan = new Scanner(System.in);
 		
 		String[][] items = new String[100][2];
-		
-		for( int i = 0; i < items.length; i++) {
+		for(int i = 0; i < items.length; i++) {
 			items[i][0] = "";
 			items[i][1] = "";
 		}
- 		
+		
 		int itemCnt = 0;
 		
-		while (true) {
-			
+		while(true) {
+
 			System.out.println("[관리자 모드]");
 			System.out.println("[1]카테고리 관리");
 			System.out.println("[2]아 이 템  관리");
@@ -47,34 +49,30 @@ public class StringEx11_정답예시 {
 			int sel = scan.nextInt();
 			
 			if (sel == 1) {
-				System.out.print("추가할 카테고리 입력 : "); //추가 카테고리 부터 설정.
+				System.out.println(" 추가할 카테고리를 입력 :");
 				String category = scan.next();
 				items[itemCnt][0] = category;
 				itemCnt++;
 			}
 			else if (sel == 2) {
-				for (int i = 0; i < itemCnt; i++) System.out.println("[" + i + "]" + items[i][0]);
-				System.out.print("카테고리를 선택하세요 : ");
-				int choice = scan.nextInt();
+				for (int i = 0; i < itemCnt; i++) System.out.println("[ "+ i + "]" + items[i][0]);
+				System.out.println(" 카테고리를 선택하십시요 : ");
+				int chice = scan.nextInt();
 				
-				System.out.print("추가할 아이템을 입력하세요 : ");
-				String item = scan.next();
-				
-				items[choice][1] += item;
-				items[choice][1] += "/";
+				System.out.println("아이템을 추가입력 : ");
+				String itemAdd = scan.next();
 			}
 			else if (sel == 3) {
 				for (int i = 0; i < itemCnt; i++) {
-					System.out.println(items[i][0] + " : " + items[i][1]);
+					System.out.println(items[i][0] + "," + items[i][1]);
 				}
-				
 			}
-			else if (sel == 4) {
-				scan.close();
-				break;
-			}
-			
+			else if (sel == 4) {}
+			break;
 		}
+
+
+
 	}
 
 }
--- 04_java_string/src/step4_01/string/StringEx11_필기2.java
+++ 04_java_string/src/step4_01/string/StringEx11_필기2.java
@@ -16,27 +16,29 @@ import java.util.Scanner;
  * 		{"음료", "콜라/"},
  * 		{"육류", "소고기/"}
  * 		...
+ * 
+ * 
  * } 
  */
 
 
-public class StringEx11_정답예시 {
+
+public class StringEx11_필기2 {
 
 	public static void main(String[] args) {
-
+		
 		Scanner scan = new Scanner(System.in);
 		
 		String[][] items = new String[100][2];
-		
-		for( int i = 0; i < items.length; i++) {
+		for(int i = 0; i < items.length; i++) {
 			items[i][0] = "";
 			items[i][1] = "";
 		}
- 		
+		
 		int itemCnt = 0;
 		
-		while (true) {
-			
+		while(true) {
+
 			System.out.println("[관리자 모드]");
 			System.out.println("[1]카테고리 관리");
 			System.out.println("[2]아 이 템  관리");
@@ -47,34 +49,37 @@ public class StringEx11_정답예시 {
 			int sel = scan.nextInt();
 			
 			if (sel == 1) {
-				System.out.print("추가할 카테고리 입력 : "); //추가 카테고리 부터 설정.
+				System.out.println("추가할 카테고리 입력 : ");
 				String category = scan.next();
-				items[itemCnt][0] = category;
-				itemCnt++;
+					items[itemCnt][0] = category;
+					itemCnt++;
+				
 			}
 			else if (sel == 2) {
 				for (int i = 0; i < itemCnt; i++) System.out.println("[" + i + "]" + items[i][0]);
-				System.out.print("카테고리를 선택하세요 : ");
+				System.out.println(" 카테고리를 선택해주세요 : ");
 				int choice = scan.nextInt();
 				
-				System.out.print("추가할 아이템을 입력하세요 : ");
-				String item = scan.next();
+				System.out.println(" 추가할 아이템을 입력 : ");
+				String itemAdd = scan.next();
 				
-				items[choice][1] += item;
+				items[choice][1] += itemAdd;
 				items[choice][1] += "/";
+				
 			}
 			else if (sel == 3) {
 				for (int i = 0; i < itemCnt; i++) {
-					System.out.println(items[i][0] + " : " + items[i][1]);
+					System.out.println(items[i][0] + "," + items[i][1]);
 				}
-				
 			}
 			else if (sel == 4) {
-				scan.close();
 				break;
 			}
 			
 		}
+
+
+
 	}
 
 }
